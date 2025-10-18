e03a9b4d34e7dde21fea3056d946ac42fcd9170f
wstrail <wstrail91@gmail.com>
2025-10-18 22:20:30 +0800

    first commit

---
+++ vue-base/src/eventdemo1.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>内联</h3>
+  <button v-on:click="addCount">Add</button>
+  <p>{{ count }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    addCount(e) {
+      e.target.innerHTML = "add" + this.count;
+      this.count++;
+    },
+  },
+};
+</script>
---
+++ vue-base/src/keydemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>key属性</h3>
+  <p v-for="(item, index) in names" :key="index">{{ item }}</p>
+</template>
+<script>
+import { ssrImportMetaKey } from "vite/module-runner";
+
+export default {
+  data() {
+    return {
+      names: ["a", "b", "c"],
+    };
+  },
+};
+</script>
