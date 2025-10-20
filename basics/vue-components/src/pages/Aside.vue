d2a9298308013cd5a74e3352f5f92d2c3ec64baf
wstrail <wstrail91@gmail.com>
2025-10-20 12:49:49 +0800

    first commit

---
+++ basics/vue-components/src/pages/Aside.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="aside">
+    <h3>Aside</h3>
+    <item />
+    <item />
+    <item />
+  </div>
+</template>
+<script>
+import item from "./item.vue";
+export default {
+  components: {
+    item,
+  },
+};
+</script>
+<style scoped>
+.aside {
+  float: right;
+  width: 30%;
+  height: 400px;
+  border: 5px solid #999;
+  box-sizing: border-box;
+}
+</style>
